815e57a6da86fa32bf2cf2b87cab665e07201401
WASEEM AHMAD MEER <120891700+MohammadWaseem6@users.noreply.github.com>
2023-07-28 19:25:08 -0800

    done

---
+++ 01_Basics/DataTypes_Summary.js
@@ -0,0 +1,18 @@
+const sahabas = ["obu Baker", "Omar", "Ali", "Osman r.a's"]; //arrays
+console.log(sahabas);
+
+
+let objct = {  // objects
+    name: "waseem",
+    age: 23,
+    dob: 11111,
+}
+console.log(objct);
+
+
+const func = function () {
+    console.log( "hello world!");
+    
+}
+func();
+func();

21923cb209a59f02e3b92cf8d4cc2c3bee718138
WASEEM AHMAD MEER <120891700+MohammadWaseem6@users.noreply.github.com>
2023-07-28 18:32:31 -0800

    first commit

---
+++ .vscode/02_comparision.js
@@ -0,0 +1,13 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 == 2);
+console.log(2 != 2);
+console.log("2" > 1);
+console.log("02" == 1);
+console.log(2 === "2");
+console.log(null == 1);
+console.log(null > 1);
+console.log(1 >= null);
+console.log(undefined==0);
+
+

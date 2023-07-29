2d4addf230a7ed8e2c6961c8f2e7ef33459a7853
WASEEM AHMAD MEER <120891700+MohammadWaseem6@users.noreply.github.com>
2023-07-28 18:34:06 -0800

    conversions

--- .vscode/02_comparision.js
+++ .vscode/02_comparision.js
@@ -8,6 +8,8 @@ console.log(2 === "2");
 console.log(null == 1);
 console.log(null > 1);
 console.log(1 >= null);
-console.log(undefined==0);
+console.log(undefined == 0);
+console.log("hello === 22");
+
 
 

f9db5fc220cdbde50b6a3a49e7c3a743286fd9d5
WASEEM AHMAD MEER <120891700+MohammadWaseem6@users.noreply.github.com>
2023-09-30 19:23:16 +0530

    project2

--- Project2/BMI.js
+++ Project2/BMI.js
@@ -1,19 +1,30 @@
 const form = document.querySelector('form');
+const result = document.querySelector('#result'); // Changed to 'result'
+const weightGuide = document.querySelector('#weight-guide'); // Added weightGuide
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
-  const height = parseInt(document.querySelector('#height').value);
-  const weight = parseInt(document.querySelector('#weight').value);
-  const result = document.querySelector('#result'); // Changed to 'result'
+  const height = parseFloat(document.querySelector('#height').value);
+  const weight = parseFloat(document.querySelector('#weight').value);
 
-  if (isNaN(height) || height <= 0) {
-    result.innerHTML = `Please give a valid height: ${height}`;
-  } else if (isNaN(weight) || weight <= 0) {
-    result.innerHTML = `Please give a valid weight: ${weight}`;
+  if (isNaN(height) || height <= 0 || isNaN(weight) || weight <= 0) {
+    result.innerHTML = 'Please provide valid height and weight.';
+    weightGuide.innerHTML = ''; // Clear weight guide if input is invalid
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    // Show the result
-    result.innerHTML = `<span>${bmi}</span>`;
+    result.innerHTML = `Your BMI: <span>${bmi}</span>`;
+    
+    let weightCategory = '';
+    if (bmi < 18.6) {
+      weightCategory = 'Underweight';
+    } else if (bmi >= 18.6 && bmi <= 24.9) {
+      weightCategory = 'Normal weight';
+    } else {
+      weightCategory = 'Overweight';
+    }
+
+    // Display weight category
+    weightGuide.innerHTML = `<h3>Weight Category:</h3><p>${weightCategory}</p>`;
   }
 });

32c898359a7e148fae6b46b4e7d902abba021da6
MohammadWaseem6 <wa6865688@gmail.com>
2024-04-24 09:50:11 +0530

    First Commit

---
+++ REVISION/Day1.js
@@ -0,0 +1,52 @@
+// js has 8 data types
+// 1 String
+// 2 Number
+// 3 BigInt
+// 4 Boolean
+// 5 undefined
+// 6 null
+// 7 Symbol
+// 8 Object
+// The Object Datatype, The object data type can contain:
+// 1. An object
+// 2. An array
+// 3. A date
+
+let name = "waseem" //String
+let w = 12; // number
+let x = false  //bolena
+let y = true;  // bolean
+
+//object
+const person = {
+    name: "waseem",
+    age: 24,
+    class: "mca"
+
+}
+//console.log(person);
+
+
+//aaray object
+const person2 = [
+    {
+        name: "waseem2",
+        age2: 252,
+        class2: "mca2"
+
+    }
+]
+//console.log(person2);
+
+// date object
+const date = new Date("2022-03-25")
+//console.log(date);
+
+
+//note : When adding a number and a string, JavaScript will treat the number as a string.
+let x1 = 16 + "Volvo";
+console.log(x1);// -> 16volvo
+
+
+//note : When adding a number and a string, JavaScript will treat the number as a string.
+
